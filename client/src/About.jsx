78239c81fd680b835827b77c0a0a0a531c4a38b4
Ryan Evanyo <ryan.evanyo@gmail.com>
2021-08-08 15:33:10 -0600

    Deleted a  line

--- client/src/About.jsx
+++ client/src/About.jsx
@@ -6,7 +6,7 @@ export default function About(props) {
             <div className="card">
                 <h2>About Me</h2>
                 <div className="fakeimg" style={{ height: 200 }}>Image</div>
-                <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+                <p>.</p>
             </div>
         </div>
     )
